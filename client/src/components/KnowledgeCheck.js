76bfeb3a42214b7f10064720c9583fee4764343d
Dylan Schultz <dylanschultz311@gmail.com>
2020-07-28 09:19:39 -0700

    Extract out anonymous functions

--- client/src/components/KnowledgeCheck.js
+++ client/src/components/KnowledgeCheck.js
@@ -8,20 +8,33 @@ export const KnowledgeCheck = ({ answers, feedback, question }) => {
     return answers[index].isCorrect;
   };
 
+  const handleSubmit = (event) => {
+    setSubmitted(true);
+    event.preventDefault();
+  };
+
+  const handleRetake = (event) => {
+    setSubmitted(false);
+    setRadioState('');
+    event.preventDefault();
+  };
+
+  const handleRadioSelect = (event) => {
+    if (!submitted) {
+      setRadioState(event.target.value);
+    }
+  };
+
   return (
     <div className="max-w-lg rounded overflow-hidden shadow-lg">
       <div className="p-12">
         <div className="font-bold text-xl mb-4">{question.text}</div>
         <img className="mb-4 w-full" src={question.media.url} alt={question.text} />
-        <form
-          onSubmit={(event) => {
-            setSubmitted(true);
-            event.preventDefault();
-          }}
-        >
+        <form onSubmit={handleSubmit}>
           {answers.map((answer, index) => (
             // Generally I wouldn't use the array index as a key, but since the radio
-            // button list is immutable there's no concern of the list changing
+            // button list is immutable there's no concern of the list changing and
+            // causing weird bugs.
             <label key={index} className="md:w-2/3 block text-gray-500 font-bold">
               <input
                 className="mr-8 leading-tight"
@@ -29,11 +42,7 @@ export const KnowledgeCheck = ({ answers, feedback, question }) => {
                 name="answers"
                 value={index}
                 checked={index === parseInt(radioState)}
-                onChange={(event) => {
-                  if (!submitted) {
-                    setRadioState(event.target.value);
-                  }
-                }}
+                onChange={handleRadioSelect}
               />
               <span className="text-gray-700 text-base">{answer.text}</span>
             </label>
@@ -53,11 +62,7 @@ export const KnowledgeCheck = ({ answers, feedback, question }) => {
               <div className="flex justify-center">
                 <button
                   className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-                  onClick={(event) => {
-                    setSubmitted(false);
-                    setRadioState('');
-                    event.preventDefault();
-                  }}
+                  onClick={handleRetake}
                 >
                   Take Again
                 </button>
